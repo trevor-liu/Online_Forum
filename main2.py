8c7ced8da77d2a695acedd7afa8437face4cb253
trevor-liu <trevorcliu@hotmail.com>
2020-11-17 00:50:51 -0800

    Task 1-3 complete

--- main2.py
+++ main2.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
+from datetime import datetime
+
+import pymongo
 
 # go to server folder and enter (mongod --dbpath=data) to start the server
+
 
 # Connecting to the mongodb
 port = int(input("Enter a port number: "))
@@ -12,6 +16,7 @@ print("Database \'291db\' is created !!")
 
 postsCol = db['Posts']
 votesCol = db['Votes']
+postsCol.create_index([('Title', pymongo.TEXT), ('Body', pymongo.TEXT), ('Tags', pymongo.TEXT)], name='search_index', default_language='english')
 
 userInputId = input("Enter your ID: ")
 
@@ -52,36 +57,88 @@ if userInputId:
 # Second Part
 run = True
 while run:
-    userAction = input("Posts question(Enter 1), Search for questions(Enter 2), Question action-Answer(Enter 3), Question action_List answers(Enter 4), Question/Answer action-Vote(Enter 5). Enter 0 to end the program: ")
-    if userAction == '0':
-        print("Ending the program...")
-        run = False
-    elif userAction == '1':
+    userAction = input("Posts question(Enter 1), Search for questions(Enter 2): ")
+    if userAction == '1':
         # Posts question
         # postsCol.create_index( { "Id":1}, unique = True)
-        # titleText = input("Enter Title: ")
-        # bodyText = input("Enter Body: ")
-        # tagText = input("Enter tags: ")
-        # postsCol.insert_one({ "PostTypeId": "1",
-        #                     "Title": titleText, 
-        #                     "Body": bodyText,
-        #                     "Tags": tagText})
+        titleText = input("Enter Title: ")
+        bodyText = input("Enter Body: ")
 
+        # Getting the tags
+        tagNumber = input("Enter the number of tags: ")
+        tagText = ""
+        for i in range(int(tagNumber)):
+            tagText = tagText + "<" + input("Enter a tag: ") + ">"
 
-        pass
+        # Creation Date
+        s = datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+        # missing Id, and havn't handle if no OwnerUserId
+        postsCol.insert_one({"PostTypeId": "1",
+                            "Title": titleText,
+                            "Body": bodyText,
+                            "Tags": tagText,
+                            "CreationDate": s[:-3],
+                            "OwnerUserId": userInputId,
+                            "Score": 0,
+                            "ViewCount": 0,
+                            "AnswerCount": 0,
+                            "CommentCount": 0,
+                            "FavoriteCount": 0,
+                            "ContentLicense": "CC BY-SA 2.5"})
+        print("Your question is posted!")
+        # Ending the program      
+        if input("Do you want to end the program?(y/n) ") == "y":
+            print("Ending the program...")
+            run = False
+
     elif userAction == '2':
+        pass
         # Search for questions
+        keyWords = input("Enter keywords seperated by space: ")
+        results = postsCol.find({"$text": {"$search": keyWords, "$caseSensitive": False}, "PostTypeId": "1"})
+        for result in results:
+    
+            # printing each question
+            print("Title: ", result["Title"])
+            print("Creation Date: ", result["CreationDate"])
+            print("Score: ", result["Score"])
+            print("Answer Count: ", result["AnswerCount"])
 
-        pass
-    elif userAction == '3':
-        # Question action-Answer
+            # user selecting a question
+            if input("Select this question?(y/n) ") == "y":
+                
+                print(result)
+                postsCol.update_one({"_id": result["_id"]}, {"$inc": {"ViewCount": 1 }})
+                print("ViewCount has been incremented.")
 
-        pass
-    elif userAction == '4':
-        # Question action_List answers
+                userQuestionAction = input("Do you want to Answer the question or view all the answer from this question:(answer/view) ")
+                if userQuestionAction == "answer":
+                    answerBody = input("Enter the body of your answer: ")
 
-        pass
-    elif userAction == '5':
-        # Question/Answer action-Vote
+                    # Creation Date
+                    s = datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
 
-        pass
+                    # ParentId, OwnerUserID and Id may be missing
+                    postsCol.insert_one({"Body": answerBody,
+                                        "PostTypeId": "2",
+                                        "CreationDate": s[:-3],
+                                        "OwnerUserId": userInputId,
+                                        "ParentId": result["Id"],
+                                        "Score": 0,
+                                        "CommentCount": 0,
+                                        "ContentLicense": "CC BY-SA 2.5"
+                                        })
+                    print("Answer posted!")
+                    if input("Do you want to view all the answer for this question?(y/n) ") == 'y': userQuestionAction = "view"
+
+                if userQuestionAction == "view":
+                    # Task 4
+                    pass
+
+
+
+
+                break;
+        
+
